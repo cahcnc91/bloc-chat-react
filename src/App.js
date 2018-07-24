6da6690d8be33cee70d363e33d53839771092123
Camila Coder <camilanierocosta@hotmail.com>
2018-07-23 22:44:39 -0400

    not working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+  var config = {
+    apiKey: "AIzaSyBCeCdMyGLpFwc28p1egBTID4Vg0xaTf1o",
+    authDomain: "bloc-chat-react-3354c.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-3354c.firebaseio.com",
+    projectId: "bloc-chat-react-3354c",
+    storageBucket: "bloc-chat-react-3354c.appspot.com",
+    messagingSenderId: "521781433743"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <RoomList firebase={firebase} />
+        </div>
       </div>
     );
   }
