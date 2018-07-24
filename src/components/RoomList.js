cac01b6ad4608115be13519a0581f24fbf0629f0
Camila Coder <camilanierocosta@hotmail.com>
2018-07-24 19:03:28 -0400

    added create new room functionality

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,10 @@ import * as firebase from 'firebase';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-    this.state = { rooms: [] };
+    this.state = { 
+      rooms: [],
+      newRoomName: ''
+    };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
@@ -12,11 +15,29 @@ class RoomList extends Component {
   componentDidMount() {
       this.roomsRef.on('child_added', snapshot => {
         const room = snapshot.val();
-        console.log(room);
         room.key = snapshot.key;
         this.setState({ rooms: this.state.rooms.concat( room ) })
       });
   }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({ newRoomName: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.createRoom(this.state.newRoomName);
+  }
+
+  createRoom(newRoomName) {
+    this.roomsRef.push({
+      name: newRoomName
+    });
+    this.setState({ newRoomName: ''});
+  }
+
+
 
   render() {
       return (
@@ -25,7 +46,15 @@ class RoomList extends Component {
             <li key={index}>
               {room.name}
             </li>
-          )}
+          )} 
+          <section className='newRoom'>
+          <h2>New room</h2>
+          <form>
+            <input type="text" id="new-room-name" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e) } />
+            <label for="new-room-name">New room's name</label>
+            <button onClick={ (e) => this.handleSubmit(e) }>Create</button>
+          </form>
+          </section>
         </div>
       );
   }
